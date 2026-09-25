9b1cb23b0346b390cabf03fe9fdf1b693a6e51df
SebasDavid14 <sebasdavid0211@gmail,com>
2026-09-24 21:05:26 -0500

    Fix: Remover @CrossOrigin duro en controladores para habilitar Railway CORS

--- src/main/java/com/sistema/backend/controllers/TaskController.java
+++ src/main/java/com/sistema/backend/controllers/TaskController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "http://localhost:4200")
 public class TaskController {
 
     private final TaskService taskService;
